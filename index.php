574979450588b3a31365c9fe958f8909a534dda0
Ewa Januszewska <ejanuszewska@onet.pl>
2016-06-12 12:11:29 +0200

    FR

---
+++ index.php
@@ -0,0 +1,139 @@
+<!DOCTYPE HTML>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<head>
+    <title>Home</title>
+    <meta charset="utf-8">    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+    <meta name = "format-detection" content = "telephone=no" />
+    <meta name="description" content="Your description">
+    <meta name="keywords" content="Your keywords">
+    <meta name="author" content="Your name">
+    <link rel="stylesheet" href="css/bootstrap.css" >
+<link rel="stylesheet" type="text/css" href="jquery.fullPage.css" />
+		<link rel="stylesheet" href="css/animations.css" type="text/css">
+
+    <link rel="stylesheet" href="style.css">
+    
+
+   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>
+    <script src="js/jquery.mobilemenu.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/jquery.ui.totop.js"></script>
+    <script src="js/jquery.touchSwipe.min.js"></script>
+
+    <script type="text/javascript" src="//use.typekit.net/rws3een.js"></script>
+<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+         
+    <script src='js/camera.js'></script>
+    <!--[if (gt IE 9)|!(IE)]><!-->
+        <script src="js/jquery.mobile.customized.min.js"></script>
+    <!--<![endif]-->
+
+ 
+
+	<script type="text/javascript" src="jquery.fullPage.js"></script>
+	<script type="text/javascript" src="examples.js"></script>
+
+    
+    <!--[if lt IE 9]>
+    <div style='text-align:center'><a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode"><img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." /></a></div>  
+    <link rel="stylesheet" href="assets/tm/css/tm_docs.css" type="text/css" media="screen">
+    <script src="assets/assets/js/html5shiv.js"></script> 
+    <script src="assets/assets/js/respond.min.js"></script>
+  <![endif]-->
+</head>
+    <body> 
+    
+
+  
+     <div class="nav">
+      <ul id="menu">
+          <li data-menuanchor="firstPage"><a href="#firstPage">About</a></li>
+          <li data-menuanchor="secondPage"><a href="#secondPage">Notification</a></li>
+          <li data-menuanchor="3rdPage"><a href="#3rdPage">Notification</a></li>
+          <li data-menuanchor="4thPage"><a href="#4thPage">Fitness</a></li>
+          <li data-menuanchor="5thPage"><a href="#5thPage">Health</a></li>
+          <li data-menuanchor="6thPage"><a href="#6thPage">Personalization</a></li>
+      </ul>
+   </div>
+    <!-- /.navbar-collapse -->
+  <!-- /.container-fluid -->
+
+       
+               <div id="fullpage">
+           <div class="im section" id="section0">
+             <div class="col-xs-12 col-md-6 vcenter nap">
+        <div><img src='img/napis.png'/></div>
+    </div><!--
+    --><div class="col-xs-12 col-md-6 vcenter zeg">
+        <div><img src='img/zegar.png'/></div>
+    </div>
+           </div>
+       
+      
+        	<div class="section" id="section1">
+	    
+
+                    <div class="container ssss"> <div class="col-md-6 col-xs-12 noth clearfix float-my-children"><img src="per.png" alt="slider" /><div class="opis"><h3>Never miss what matters.</h3>
+Never miss what matters. Like anyone, you have notifications that are important to you. But often they arrive at times when it’s inconvenient — or impossible — to reach for your phone. With Apple Watch, the information you care about most comes right to your wrist.</div></div>
+                            <div class="col-md-6 col-xs-12 health clearfix float-my-children"> <img src="heal.png" alt="slider" /><h3>Stay healthy.</h3>
+                                    Throughout the day, Apple Watch helps encourage healthy routines. All adding up to a better you.</div>
+                        <div class="col-md-6 col-xs-12 run clearfix float-my-children"> <img src="run.png" alt="slider" /><h3>Stay motivated.</h3>
+Throughout the day, Apple Watch measures your workouts and tracks your activity.</div>
+                            <div class="col-md-6 col-xs-12 org clearfix float-my-children"> <img src="org.png" alt="slider" /><h3>Choose yours. Then make it even more you.</h3>
+                                    We designed Apple Watch to reflect a wide range of tastes and styles. And you can make it even more personal by switching bands and changing watch faces, anytime you want.</div></div>
+			</div>
+            
+        <div class="section" id="section2">
+	    
+			
+
+                            <div class="col-md-6 slide1"></div><div class="col-md-6 col-xs-12 tyd">dfdf</div>
+			
+            </div> 
+        <div class="section" id="section3">
+	  
+  <div class="col-md-6 col-xs-12 tyd">tt</div>
+			         <div class="col-md-6 slide2"></div>
+            </div>
+                     <div class="section" id="section4">
+	  
+			         <div class="col-md-6 slide3"></div>
+			<div class="col-md-6">tt</div>
+            </div> 
+                       <div class="section" id="section5">
+	  <div class="col-md-6">tt</div>
+			         <div class="col-md-6 slide4"></div>
+			
+            </div> 
+                        </div>
+       </div>
+  
+        <?php
+        // put your code here
+        ?>
+        
+        <script src="js/bootstrap.min.js"></script>
+<script src="js/tm-scripts.js"></script>
+
+	<script type="text/javascript">
+		$(document).ready(function() {
+			$('#fullpage').fullpage({
+				sectionsColor: ['rgba(200, 54, 54, 0.0)', 'white', 'white', 'whitesmoke', 'white', '#ccddff'],
+				anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage','5thPage', '6thPage', 'lastPage'],
+				
+				scrollingSpeed: 1000
+			});
+
+		});
+	</script>
+<script src='js/css3-animate-it.js'></script>
+    </body>
+</html>
